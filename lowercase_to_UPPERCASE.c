1216f12677b3521594b510f603cb2f28bf1bc3f8
shaswatdwivedi <shasvedi13@gmail.com>
2026-01-12 15:49:18 +0530

    Added pointer programs

---
+++ lowercase_to_UPPERCASE.c
@@ -0,0 +1,20 @@
+/*
+Problem - lowercase to UPPERCASE
+Platform - HackerRank
+Summary - Converting lowercase into UPPERCASE
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char charLower;
+    char charUpper;
+    scanf("%c",&charLower);
+    charUpper = charLower - 32;
+    printf("The uppercase of %c is %c ",charLower,charUpper);
+   
+    return 0;
+}
