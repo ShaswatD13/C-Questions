3d7b2c061c4255fd4396dfd47a1828ad09202cf8
shaswatdwivedi <shasvedi13@gmail.com>
2026-01-12 15:53:00 +0530

    Added pointer programs

---
+++ Sum_of_two_numbers.c
@@ -0,0 +1,19 @@
+/*
+Problem - Sum Of Two Numbers
+Platform - HackerRank
+Summary - Printing Sum Of Two Numbers
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,sum;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    sum = a+b;
+    printf("The sum of %d and %d is %d.",a,b,sum);
+    return 0;
+}
