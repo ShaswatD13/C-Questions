081d087bf90306aac4e2d630a9f8a8b49a2683ec
shaswatdwivedi <shasvedi13@gmail.com>
2026-01-12 14:59:41 +0530

    Added pointer programs

---
+++ Hello_World!.c
@@ -0,0 +1,16 @@
+/*
+Problem - Hello World!
+Platform - HackerRank
+Summary - Printing Hello World.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World!");
+  
+    return 0;
+}
