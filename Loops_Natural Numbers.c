6b5b37409cbab4ff5870599a227060cfa028b6b0
shaswatdwivedi <shasvedi13@gmail.com>
2026-01-27 20:40:19 +0530

    Added pointer programs

--- Loops_Natural Numbers.c
+++ Loops_Natural Numbers.c
@@ -3,3 +3,15 @@ Problem - Loops_Natural Numbers
 Platform - HackerRank
 Summary - Write a program that takes a positive integer N as input from the user and prints all natural numbers from 1 to N, with each number followed by a space (including the last number).
 */
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    for(int i = 1;i<=N;++i)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
