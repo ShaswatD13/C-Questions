a3ef4fb4a288931dc0e8be1a63829783032ff171
shaswatdwivedi <shasvedi13@gmail.com>
2026-02-03 21:04:04 +0530

    Added pointer programs

---
+++ Loops_Alternate Positive/Pattern_Reverse Number Stair Pattern.c
@@ -0,0 +1,27 @@
+/*
+Problem - Pattern_Reverse Number Stair Pattern
+Platform - HackerRank
+Summary - Your task is to print a reverse number stair pattern using numbers from N to 1.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+   for(int i=N;i>=1;--i)
+    {
+        for(int j=i;j>=1;--j)
+        {
+        printf("%d ",j);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    return 0;
+}
