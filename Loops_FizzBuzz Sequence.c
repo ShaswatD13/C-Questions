c988d11d90180f904a1f5f4f1417b453f74466d9
shaswatdwivedi <shasvedi13@gmail.com>
2026-02-08 21:24:39 +0530

    Added pointer programs

---
+++ Loops_FizzBuzz Sequence.c
@@ -0,0 +1,26 @@
+/*
+Problem - Loops_FizzBuzz Sequence
+Platform - HackerRank
+Summary - Write a program that prints a sequence of values from 1 to N following the rules of the classic FizzBuzz problem.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i=1;i<=N;++i) {
+        
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
