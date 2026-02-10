430d1fd3c574207c78e83ea4f9a82aa226c3e972
shaswatdwivedi <shasvedi13@gmail.com>
2026-02-10 21:24:26 +0530

    Added pointer programs

---
+++ Loops_Reverse Odd Numbers.c
@@ -0,0 +1,37 @@
+/*
+Problem - Loops_Reverse Odd Numbers
+Platform - HackerRank
+Summary - Write a program to print the first n odd numbers, starting from 1, in reverse order. The odd numbers should be printed starting from the largest odd number in the sequence, moving backwards to the smallest odd number.
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    
+    if(n!=0)
+    {
+    
+    int a[n];
+    for(int i = 0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(int i = 0;i < n;++i)
+    {
+        a[i] = 2*i + 1;
+    }
+    
+    for(int i = n-1; i > 0 ; --i)
+    {
+            printf("%d ",a[i]);
+    }
+    printf("1.");
+    }
+    
+
+    return 0;
+}
