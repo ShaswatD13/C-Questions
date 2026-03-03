5c564077eedb6fc992ceaf277cf3a5519b66ee5c
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-03 21:17:12 +0530

    Added pointer programs

---
+++ PredictQ6.c
@@ -0,0 +1,19 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4};
+    int *p = arr;
+    
+    printf("%d ", *p);
+    printf("%d ", *p++);
+    printf("%d ", (*p)++);
+    printf("%d ", *p);
+
+    return 0;
+}
+
+// 1 1 2 3
