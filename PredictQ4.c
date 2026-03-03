f3c9164976a58ec6120cbc5a756e27e547db879a
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-03 21:12:58 +0530

    Added pointer programs

---
+++ PredictQ4.c
@@ -0,0 +1,15 @@
+/*
+Problem - What will be the output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 4;
+    int c = a+++b;
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
+
+// 4 4 7
