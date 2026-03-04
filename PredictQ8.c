b1145bdf8f7755a723035d6f5ec0baf10717ac02
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-04 22:54:15 +0530

    Added pointer programs

---
+++ PredictQ8.c
@@ -0,0 +1,15 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    char ch = 'A';
+    printf("%c ", ch + 1);
+    printf("%d ", ch);
+    printf("%c", ch + 2);
+    return 0;
+}
+
+// B 65 C
