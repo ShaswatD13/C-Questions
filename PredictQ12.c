3e0f50fa2547854b8b3fefb2df7166b9667d2f54
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-05 18:26:45 +0530

    Added pointer programs

---
+++ PredictQ12.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while(i <= 3)
+    {
+        printf("%d ", i);
+        i++;
+    }
+
+    printf("%d", i);
+
+    return 0;
+}
+
+// 1 2 3 4
