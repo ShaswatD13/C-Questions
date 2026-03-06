aee06d9ddba06e01c740e08f17b3c4b17b7bb45c
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-06 16:30:49 +0530

    Added pointer programs

---
+++ PredictQ15.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 5;
+
+    if(a && b)
+        printf("Hello");
+    else
+        printf("World");
+
+    return 0;
+}
+
+// World
