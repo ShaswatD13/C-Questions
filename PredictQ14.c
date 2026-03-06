fa0666eec620e25de0518305c4c5d6db07eba8ae
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-06 16:29:46 +0530

    Added pointer programs

---
+++ PredictQ14.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for(i = 1; i <= 5; i++)
+    {
+        if(i == 4)
+            break;
+
+        printf("%d ", i);
+    }
+
+    return 0;
+}
+
+// 1 2 3
