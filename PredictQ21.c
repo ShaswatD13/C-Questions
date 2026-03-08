22aebc43593477bacbf2ca72de911e539ab08846
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-08 19:46:24 +0530

    Added pointer programs

---
+++ PredictQ21.c
@@ -0,0 +1,17 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+
+    printf("%d ", a++);
+    printf("%d ", ++a);
+    printf("%d", a);
+
+    return 0;
+}
+
+// 3 5 5
