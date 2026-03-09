911f78f96dbf4276887e1b342f83e66465576e3d
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-09 21:01:56 +0530

    Added pointer programs

---
+++ PredictQ23.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 3;
+
+    int c = a + b * a + b;
+
+    printf("%d", c);
+
+    return 0;
+}
+
+// 11
