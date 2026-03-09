b2d1992dab9daa1b77b359142ed008cb8d9c897e
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-09 21:06:04 +0530

    Added pointer programs

---
+++ PredictQ25.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 7;
+
+    if(a & 1)
+        printf("Odd");
+    else
+        printf("Even");
+
+    return 0;
+}
+
+// Odd
