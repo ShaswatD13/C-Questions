e1da6e53b9390ecd16a1869013ca5c68725b3b58
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-10 23:04:30 +0530

    Added pointer programs

---
+++ PredictQ27.c
@@ -0,0 +1,14 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main() {
+    int x = 4;
+    int y = x-- * 2 + ++x;
+    printf("%d %d", x, y);
+    return 0;
+}
+
+// 4 12
