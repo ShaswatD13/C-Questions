0a21e83c2e3b82b769dbc7ec86cf9f4c42e66c61
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-15 21:06:33 +0530

    Added pointer programs

---
+++ PredictQ43.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'A';
+
+    printf("%c ", ch + 3);
+    printf("%d ", ch);
+    printf("%c", ch + 32);
+
+    return 0;
+}
+
+// D 65 a
