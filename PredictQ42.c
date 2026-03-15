5101c93c6a25606f222fb56f2e14bae86af19ab9
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-15 21:04:20 +0530

    Added pointer programs

---
+++ PredictQ42.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int *p = &x;
+
+    *p = *p + 5;
+    printf("%d %d", x, *p);
+
+    return 0;
+}
+
+// 15 15
