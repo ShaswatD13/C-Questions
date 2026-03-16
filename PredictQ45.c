9c9807e4799a6bee38cff864ae38d728a4244c5a
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-16 21:01:57 +0530

    Added pointer programs

---
+++ PredictQ45.c
@@ -0,0 +1,22 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+void fun()
+{
+    static int x = 1;
+    x++;
+    printf("%d ", x);
+}
+
+int main()
+{
+    fun();
+    fun();
+    fun();
+    return 0;
+}
+
+// 2 3 4
