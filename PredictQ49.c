00af4cfe6061e1ba5ccc411ab421047bc2229ed6
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-17 21:58:29 +0530

    Added pointer programs

---
+++ PredictQ49.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c;
+
+    c = a && ++b;
+    printf("%d %d %d\n", a, b, c);
+
+    c = a || ++b;
+    printf("%d %d %d", a, b, c);
+
+    return 0;
+}
+
+// 0 1 0
+// 0 2 1
