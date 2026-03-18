667cb56a433e6e845e308d33d754bb0e1321d93d
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-18 20:51:10 +0530

    Added pointer programs

---
+++ PredictQ52.c
@@ -0,0 +1,27 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 2;
+
+    switch(x)
+    {
+        case 1:
+            printf("A ");
+        case 2:
+            printf("B ");
+        case 3:
+            printf("C ");
+            break;
+        default:
+            printf("D ");
+    }
+
+    return 0;
+}
+
+// B C
