3c12a39afb02fa2276b83744d662a25636722136
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-18 20:49:48 +0530

    Added pointer programs

---
+++ PredictQ50.c
@@ -0,0 +1,20 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+void modify(int x)
+{
+    x = x + 10;
+}
+
+int main()
+{
+    int a = 5;
+    modify(a);
+    printf("%d", a);
+    return 0;
+}
+
+// 5
