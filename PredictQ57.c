2f200418cc33edc14c02991750dae3b7568b783e
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-20 22:32:55 +0530

    Added pointer programs

---
+++ PredictQ57.c
@@ -0,0 +1,20 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+
+    do
+    {
+        printf("%d ", i);
+        i--;
+    } while(i < 3);
+
+    return 0;
+}
+
+// 5
