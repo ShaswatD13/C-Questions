b073af75f0411b318ceeced9dff2fb683b772d72
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-21 18:16:35 +0530

    Added pointer programs

---
+++ PredictQ59.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "Hello";
+
+    printf("%lu ", sizeof(str));
+    printf("%lu", strlen(str));
+
+    return 0;
+}
+
+// 6 5
