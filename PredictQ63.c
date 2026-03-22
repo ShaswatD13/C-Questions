dd7b62f52a13902022d23f09c6ca736b8d8163ff
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-22 22:13:14 +0530

    Added pointer programs

---
+++ PredictQ63.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 10;
+
+    int max = (a > b) ? a : b;
+
+    printf("%d", max);
+
+    return 0;
+}
+
+// 10
