18bf4b320c1e2374913b2d71325402ca62007d5f
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-22 22:12:16 +0530

    Added pointer programs

---
+++ PredictQ62.c
@@ -0,0 +1,20 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char str1[] = "Hi";
+    char *str2 = "Hi";
+
+    str1[0] = 'B';
+    // str2[0] = 'B';  // (not allowed)
+
+    printf("%s %s", str1, str2);
+
+    return 0;
+}
+
+// Bi Hi
