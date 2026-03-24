e207618f0bb356df617c7da906179cd19770ccd0
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-24 22:18:28 +0530

    Added pointer programs

---
+++ PredictQ68.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40};
+
+    printf("%d ", arr[1]);
+    printf("%d ", 2[arr]);
+    printf("%d", *(arr + 3));
+
+    return 0;
+}
+
+// 20 30 40
