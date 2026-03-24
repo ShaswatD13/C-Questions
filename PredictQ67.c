060eca387c4e477e73af908e966ed478c2f20738
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-24 22:17:32 +0530

    Added pointer programs

---
+++ PredictQ67.c
@@ -0,0 +1,20 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+void change(int *p)
+{
+    *p = *p + 20;
+}
+
+int main()
+{
+    int a = 5;
+    change(&a);
+    printf("%d", a);
+    return 0;
+}
+
+// 25
