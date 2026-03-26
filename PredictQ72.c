376c28b0d5d41d6dadc8c01dbe95743e2b5fe17b
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-26 12:31:50 +0530

    Added pointer programs

---
+++ PredictQ72.c
@@ -0,0 +1,16 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    printf("%d %d %d", i++, ++i, i++);
+
+    return 0;
+}
+
+// 1 3 3 OR 2 3 3 
