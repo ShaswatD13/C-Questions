72fde15e52f692aece87cc9865e60093f1978da0
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-27 19:40:15 +0530

    Added pointer programs

---
+++ PredictQ75.c
@@ -0,0 +1,17 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 8;
+
+    printf("%d ", x >> 1);
+    printf("%d", x << 2);
+
+    return 0;
+}
+
+// 4 32
