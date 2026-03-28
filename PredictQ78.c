9cc02f04812b30822f5fb450c56f6d54e0821146
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-28 22:25:43 +0530

    Added pointer programs

---
+++ PredictQ78.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 0;
+
+    if(a = b)
+        printf("Yes ");
+    else
+        printf("No ");
+
+    printf("%d", a);
+
+    return 0;
+}
+
+// No 0
