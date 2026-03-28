7b98ae2f7e37df6516f0bc70b3a519fa6e698035
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-28 22:23:53 +0530

    Added pointer programs

---
+++ PredictQ76.c
@@ -0,0 +1,25 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+void fun()
+{
+    static int x = 1;
+
+    if(x <= 3)
+    {
+        printf("%d ", x);
+        x++;
+        fun();
+    }
+}
+
+int main()
+{
+    fun();
+    return 0;
+}
+
+// 1 2 3
