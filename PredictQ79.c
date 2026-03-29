f5675026150adecd28eb9029b8ef8fe0956f0ad4
shaswatdwivedi <shasvedi13@gmail.com>
2026-03-29 20:26:40 +0530

    Added pointer programs

---
+++ PredictQ79.c
@@ -0,0 +1,17 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {10, 20, 30};
+    int *p = arr;
+    int *q = arr + 2;
+
+    printf("%d ", p < q);
+    printf("%d", p == arr);
+
+    return 0;
+}
