c6e4bf96eb032fbba3ecd62f8dfef6d661fbfc78
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-01 21:01:52 +0530

    Added pointer programs

---
+++ PredictQ88.c
@@ -0,0 +1,19 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {2, 4, 6, 8, 10};
+    int *p = arr + 1;
+
+    printf("%d ", *(p + 2));
+    printf("%d ", *(arr + 4));
+    printf("%d", *(p - 1));
+
+    return 0;
+}
+
+// 10 8 2
