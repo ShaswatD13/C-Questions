53596f9db27274fbae82a559b74486c5cccda968
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-01 21:02:25 +0530

    Added pointer programs

---
+++ PredictQ89.c
@@ -0,0 +1,26 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 1, b = 0, c = 1;
+
+    if(a && b || c)
+    {
+        if(!b)
+            printf("X ");
+        else
+            printf("Y ");
+    }
+    else
+    {
+        printf("Z ");
+    }
+
+    return 0;
+}
+
+// X
