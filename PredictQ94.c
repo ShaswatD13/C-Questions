36a6a918e5dd39dc5c36cd081b83c60ef297c6d2
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-04 21:15:02 +0530

    Added pointer programs

---
+++ PredictQ94.c
@@ -0,0 +1,18 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 10, c = 15;
+
+    int result = (a > b) ? a : (b > c) ? b : c;
+
+    printf("%d", result);
+
+    return 0;
+}
+
+// 15
