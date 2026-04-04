45d792c787b1b21bb6ca7c32f83b682091f97063
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-04 21:16:08 +0530

    Added pointer programs

---
+++ PredictQ96.c
@@ -0,0 +1,21 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    while(i = i + 1)
+    {
+        printf("%d ", i);
+        if(i == 3)
+            break;
+    }
+
+    return 0;
+}
+
+// 1 2 3
