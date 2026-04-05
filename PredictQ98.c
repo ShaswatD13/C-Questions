d29ec7f83094a5c64514aa98af240d50e6023f6a
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-05 20:30:19 +0530

    Added pointer programs

---
+++ PredictQ98.c
@@ -0,0 +1,23 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+
+    if(a != 0 && 10 / a > 1)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
+
+// No
