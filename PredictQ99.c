7de3d0c8a00127647b6c6f9da824d11a20805d8b
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-06 19:55:41 +0530

    Added pointer programs

---
+++ PredictQ99.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int *p = arr;
+
+    printf("%d ", *++p);
+    printf("%d ", *p++);
+    printf("%d", *p);
+
+    return 0;
+}
+
+// 2 2 3
