bd5ef5ead049e73bce91142033248063b070d811
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-11 22:25:58 +0530

    Added pointer programs

---
+++ PredictQ103.c
@@ -0,0 +1,23 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int fun()
+{
+    static int x = 0;
+    x++;
+    return x;
+}
+
+int main()
+{
+    printf("%d ", fun());
+    printf("%d ", fun() + fun());
+    printf("%d", fun());
+
+    return 0;
+}
+
+// Undefined Behavior (due to multiple function calls without sequence)
