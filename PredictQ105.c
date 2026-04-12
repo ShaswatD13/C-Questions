0bda455342722c7a135586fffed8f7fa94fd2821
shaswatdwivedi <shasvedi13@gmail.com>
2026-04-12 22:52:35 +0530

    Added pointer programs

---
+++ PredictQ105.c
@@ -0,0 +1,19 @@
+/*
+Problem - What will be the Output?
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    while(++i < 4)
+    {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
+
+// 1 2 3
